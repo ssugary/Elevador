e9a16bc2fcb1a56e4a704a7d1a5ddfcd2f8e5933
ssugary <ssugary0@gmail.com>
2025-10-17 11:07:13 -0300

    comentários :D

--- teclado.vhd
+++ teclado.vhd
@@ -5,61 +5,61 @@ USE IEEE.NUMERIC_STD.ALL;
 
 entity Keyboard is
     port (
-       botoes_in : in std_logic_vector(31 DOWNTO 0);
-       andarAtual_in : in std_logic_vector(4 DOWNTO 0);
-       porta_in : in std_logic;
-       direcao_in : in std_logic;
+       botoes_in : in std_logic_vector(31 DOWNTO 0);        -- vetor de todos os botões
+       andarAtual_in : in std_logic_vector(4 DOWNTO 0);     -- vetor com o dígito do andar atual
+       direcao_in : in std_logic;                           -- direção do elevador (0 -> baixo, 1 -> cima)
 
-       proximoAndar_out : out std_logic_vector(4 DOWNTO 0);
-       direcao_out : out std_logic
+       proximoAndar_out : out std_logic_vector(4 DOWNTO 0); -- vetor com o dígito do próximo andar
+       direcao_out : out std_logic                          -- direção do elevador (pode mudar dependendo do próximo andar)
        
     );
 end entity Keyboard;
 
 architecture behavior of Keyboard is 
 
-    CONSTANT ULTIMO_ANDAR : INTEGER := 31;    
+    CONSTANT ULTIMO_ANDAR : INTEGER := 31;     
     
     begin
-        process (botoes_in, andarAtual_in, porta_in, direcao_in)
-        VARIABLE ANDAR_ATUAL   : INTEGER := TO_INTEGER(UNSIGNED(andarAtual_in));
-        VARIABLE ANDAR_ENCONTRADO : BOOLEAN := FALSE;
+        process (botoes_in, andarAtual_in, direcao_in)
+        VARIABLE ANDAR_ATUAL   : INTEGER := TO_INTEGER(UNSIGNED(andarAtual_in)); -- Transformação do vetor em inteiro
+        VARIABLE ANDAR_ENCONTRADO : BOOLEAN := TRUE;                            -- Booleano que verifica se temos algum andar para ir
         begin
            
         
-            if (botoes_in = (others => '0')) then
-                proximoAndar_out <= andarAtual_in;
-                direcao_out <= direcao_in;
-                ANDAR_ENCONTRADO := TRUE;
+            if (botoes_in = (others => '0')) then           -- Verifica se algum botão foi apertado
+                proximoAndar_out <= andarAtual_in;          -- se não, define o próximo andar como o atual 
+                direcao_out <= direcao_in;                   
+                ANDAR_ENCONTRADO := FALSE;
             end if;
 
-            
-            if not ANDAR_ENCONTRADO then
-                if direcao_in = '1' then                           -- 1 cima, 0 baixo
+
+            if ANDAR_ENCONTRADO then                               -- Se achou alguém, vai verificar com base na direção
+                if direcao_in = '1' then                           -- Caso a direção for para cima, vai conferir cada andar acima
                     for i in ANDAR_ATUAL + 1 to ULTIMO_ANDAR loop
                         if botoes_in(i) = '1' then
                             proximoAndar_out <= STD_LOGIC_VECTOR(TO_UNSIGNED(i, proximoAndar_out'LENGTH));
                             direcao_out <= direcao_in;
                             ANDAR_ENCONTRADO := TRUE;
-                            exit;
-                        end if;
+                            exit;                               -- Define que o retorno vai ser o primeiro andar encontrado e break
+                        end if;         
                     end loop;
                     
-                    if not ANDAR_ENCONTRADO then
+                    if not ANDAR_ENCONTRADO then                -- Caso não ache nada acima, verifica todos os abaixo
                         for i in ANDAR_ATUAL - 1 downto 0 loop
                             if botoes_in(i) = '1' then
                                 proximoAndar_out <= STD_LOGIC_VECTOR(TO_UNSIGNED(i, proximoAndar_out'LENGTH));
                                 direcao_out <= not direcao_in;
-                                ANDAR_ENCONTRADO := TRUE;
-                                exit;
+                                ANDAR_ENCONTRADO := TRUE;       -- Caso ache, troca a direção e define que o retorno vai ser o primeiro
+                                                                -- encontrado.
+                                exit;                           
                             end if;
                         end loop;
                     end if;
 
                 else 
 
-                    for i in ANDAR_ATUAL - 1 downto 0 loop
-                        if botoes_in(i) = '1' then
+                    for i in ANDAR_ATUAL - 1 downto 0 loop      -- similar a lógica do último, mas faz o contrário
+                        if botoes_in(i) = '1' then              
                             proximoAndar_out <= STD_LOGIC_VECTOR(TO_UNSIGNED(i, proximoAndar_out'LENGTH));
                             direcao_out <= direcao_in;
                             ANDAR_ENCONTRADO := TRUE;
